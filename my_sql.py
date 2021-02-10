62d6258d5b8159b79aaa527eab10f8d136913bf9
cSDes1gn <christian911@sympatico.ca>
2021-02-10 12:37:07 -0500

    mariadb integration

--- my_sql.py
+++ my_sql.py
@@ -1,50 +1,27 @@
-import mysql.connector
-from datetime import datetime
+import mariadb
+import sys
 
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="&ZX^UB&7vykP",
-    database="testdatabase"
-)
+# Adds contact
+def add_contact(cursor, first_name, last_name, email):
+   """Adds the given contact to the contacts table"""
+   cursor.execute("INSERT INTO test.contacts(first_name, last_name, email) VALUES (?, ?, ?)",
+      (first_name, last_name, email))
 
-users = [("tim", "tech"),
-         ("alan", "lana"),
-         ("sara", "saraaa")]
-
-user_scores = [(45,100),
-               (30,40),
-               (46,126)]
+# Instantiate Connection
+try:
+    db = mariadb.connect(
+            user="connpy_test",
+            password="passwd",
+            host="localhost",
+            port=3306
+        )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
 
 cursor = db.cursor()
-# cursor.execute("CREATE TABLE Test (id int PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(50) NOT NULL, created datetime NOT NULL, gender ENUM('M', 'F') NOT NULL)")
-# cursor.execute ("INSERT INTO Test (name, created, gender) VALUES (%s,%s,%s)", ("gio", datetime.now(), "M"))
-# db.commit()
-# cursor.execute("SELECT * FROM Test WHERE gender = 'M' ORDER BY id ASC")
-# cursor.execute("ALTER TABLE Test ADD COLUMN food VARCHAR(50) NOT NULL")
-# cursor.execute("ALTER TABLE Test DROP COLUMN food")
-# cursor.execute("ALTER TABLE Test CHANGE name first_name VARCHAR(50)")
-# cursor.execute("DESCRIBE Test")
 
-# Q1 = "CREATE TABLE Users (id int PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), passwd VARCHAR(50))"
-# Q2 = "CREATE TABLE Scores (userId int PRIMARY KEY, FOREIGN KEY(userId) REFERENCES Users(id), game1 int DEFAULT 0, game2 int DEFAULT 0)"
-# cursor.execute(Q1)
-# cursor.execute(Q2)
-# cursor.execute("SHOW TABLES")
-Q3 = "INSERT INTO Users (name, passwd) VALUES (%s,%s)"
-Q4 = "INSERT INTO Scores (userId, game1, game2) VALUES (%s,%s,%s)"
+add_contact(cursor, "Christian", "Sargusingh", "rqpegasus@gmail.com")
 
-for x, usr in enumerate(users):
-    cursor.execute(Q3, usr)
-    last_id = cursor.lastrowid
-    cursor.execute(Q4, (last_id,) + user_scores[x])
-
-db.commit()
-
-cursor.execute("SELECT * FROM Users")
-for x in cursor:
-    print(x)
-
-cursor.execute("SELECT * FROM Scores")
-for x in cursor:
-    print(x)
+# Close Connection
+db.close()
