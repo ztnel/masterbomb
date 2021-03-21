99f53eb64a6563ce7086eb1eddb6f8d51f73d425
cSDes1gn <christian911@sympatico.ca>
2021-03-21 18:07:48 -0400

    render ejs home page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
+import express from 'express';
 import { Router } from 'express';
 import usersRouter from './users.routes';
 
@@ -5,6 +6,11 @@ const routes = Router();
 
 // connect index router to /users route
 routes.use('/users', usersRouter);
+
+/* GET home page */
+routes.get('/', (request:express.Request, response:express.Response, next:express.NextFunction) => {
+  response.render('index', { title: 'Masterbomb' });
+});
 
 // export module
 export default routes;
