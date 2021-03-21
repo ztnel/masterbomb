80da700812e0ae11de083b7dfb798ed8a4ddb2ce
cSDes1gn <christian911@sympatico.ca>
2021-03-21 16:48:01 -0400

    sample route

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import { Router } from 'express';
+
+const usersRouter = Router();
+
+usersRouter.get('/', (request:express.Request, response:express.Response) => {
+  response.send("Hello from users route");
+});
+
+export default usersRouter;
