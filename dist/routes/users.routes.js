79ef482884cabb10be334edde6d10863f1bba609
Christian Sargusingh <christiansargusingh@cmail.carleton.ca>
2021-03-22 21:12:28 -0400

    Delete dist directory

--- dist/routes/users.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const usersRouter = express_1.Router();
-usersRouter.get('/', (request, response) => {
-    response.send("Hello from users route");
-});
-exports.default = usersRouter;
-//# sourceMappingURL=users.routes.js.map
