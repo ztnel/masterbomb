ec6891d482f3bd70fd588593b65eee7a086c177a
cSDes1gn <christian911@sympatico.ca>
2021-03-23 00:02:03 -0400

    setup public main.ts front facing scripts

---
+++ src/public/js/main.ts
@@ -0,0 +1,10 @@
+
+// data array for populating suppliers
+let suppliers = [];
+
+// DOM Ready
+const form = document.querySelector('form');
+
+form?.addEventListener('submit', async event => {
+    event.preventDefault();
+});
