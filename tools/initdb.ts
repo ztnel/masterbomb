1730d72c0e5cc2ce989041722a59730287a51c4b
cSDes1gn <christian911@sympatico.ca>
2021-03-30 03:06:07 -0400

    update psql init

--- tools/initdb.ts
+++ tools/initdb.ts
@@ -10,14 +10,26 @@ const init = async () => {
     const client = new Client();
     try {
         await client.connect();
+        /* Add custom psql triggers in seperate query to reduce parsing complexity */
+        // create automatic timestamp generation for updated_at and created_at fields
+        await client.query(`
+            CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+            RETURNS TRIGGER AS $$
+            BEGIN
+                NEW.updated_at = NOW();
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;`
+        );
         // read in sql
         const sql = await fs.readFile("./tools/initdb.sql", { encoding: "UTF-8" });
         // parse the sql file and execute statements one at a time
         const statements = sql.split( /;\s*$/m );
         for ( const statement of statements ) {
             if ( statement.length > 3 ){
-                // execute each of the statements
-                await client.query(statement)
+                // execute each of the statements and log sql queries
+                console.log(statement);
+                await client.query(statement);
             }
         }
     } catch ( err ) {
