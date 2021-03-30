a3658a661cd66f9e9bafc4d653b2084c9922a214
cSDes1gn <christian911@sympatico.ca>
2021-03-30 03:05:48 -0400

    update sql timestamps

--- tools/initdb.sql
+++ tools/initdb.sql
@@ -1,17 +1,27 @@
 -- Drop existing tables with same name
-drop table if exists suppliers;
-drop table if exists projects;
-drop table if exists manufacturers;
-drop table if exists parts;
+drop table if exists suppliers cascade;
+drop table if exists projects cascade;
+drop table if exists manufacturers cascade;
+drop table if exists parts cascade;
 
+-- --- define auto trigger event for timestamp fields
+-- create or replace function trigger_set_timestamp()
+-- returns trigger
+-- language plpgsql as
+-- '
+-- BEGIN
+--     NEW.updated_at = now();
+--     RETURN NEW;
+-- END;
+-- ';
 
--- create 'suppliers' table
+--- create 'suppliers' table
 create table if not exists suppliers (
     id int not null primary key generated always as identity,
     name varchar(255) not null,
     website varchar(255) null,
-    updated_at timestamp not null,
-    created_at timestamp not null
+    updated_at timestamp not null default now(),
+    created_at timestamp not null default now()
 );
 
 --- create 'projects' table
@@ -19,16 +29,16 @@ create table if not exists projects (
     id int not null primary key generated always as identity,
     name varchar(255) not null,
     description varchar(255) null,
-    updated_at timestamp not null,
-    created_at timestamp not null
+    updated_at timestamp not null default now(),
+    created_at timestamp not null default now()
 );
 
 --- create 'manufacturers' table
 create table if not exists manufacturers (
     id int not null primary key generated always as identity,
     name varchar(255) not null,
-    updated_at timestamp not null,
-    created_at timestamp not null
+    updated_at timestamp not null default now(),
+    created_at timestamp not null default now()
 );
 
 --- create 'parts' table
@@ -45,10 +55,9 @@ create table if not exists parts (
         foreign key(id)
             references suppliers(id)
             on delete set null,
-    updated_at timestamp not null,
-    created_at timestamp not null
+    updated_at timestamp not null default now(),
+    created_at timestamp not null default now()
 );
-
 
 --- create 'bom' table (for many to many parts and projects resolution)
 create table if not exists bom (
@@ -57,5 +66,40 @@ create table if not exists bom (
         foreign key(id)
             references projects(id)
             on delete cascade,
-    created_at timestamp not null
+    updated_at timestamp not null default now(),
+    created_at timestamp not null default now()
 );
+
+--- drop triggers if exists
+drop trigger if exists set_timestamp on manufacturers;
+drop trigger if exists set_timestamp on suppliers;
+drop trigger if exists set_timestamp on parts;
+drop trigger if exists set_timestamp on bom;
+drop trigger if exists set_timestamp on projects;
+
+
+--- register trigger function for all tables
+create trigger set_timestamp
+before update on manufacturers
+for each row
+execute procedure trigger_set_timestamp();
+
+create trigger set_timestamp
+before update on suppliers
+for each row
+execute procedure trigger_set_timestamp();
+
+create trigger set_timestamp
+before update on parts
+for each row
+execute procedure trigger_set_timestamp();
+
+create trigger set_timestamp
+before update on bom
+for each row
+execute procedure trigger_set_timestamp();
+
+create trigger set_timestamp
+before update on projects
+for each row
+execute procedure trigger_set_timestamp();
