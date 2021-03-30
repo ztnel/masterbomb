d6cacf75e90378274637053d9af039c239e01c03
cSDes1gn <christian911@sympatico.ca>
2021-03-30 01:35:30 -0400

    fix table init sequencing

--- tools/initdb.sql
+++ tools/initdb.sql
@@ -5,24 +5,6 @@ drop table if exists manufacturers;
 drop table if exists parts;
 
 
---- create 'parts' table
-create table if not exists parts (
-    id int not null primary key generated always as identity,
-    name varchar(255) not null,
-    unit_price decimal(10,2),
-    description varchar(255) null,
-    constraint fk_manufacturer
-        foreign key(id)
-            references manufacturers(id)
-            on delete set null,
-    constraint fk_suppliers
-        foreign key(id)
-            references suppliers(id)
-            on delete set null,
-    updated_at timestamp not null,
-    created_at timestamp not null
-);
-
 -- create 'suppliers' table
 create table if not exists suppliers (
     id int not null primary key generated always as identity,
@@ -48,6 +30,25 @@ create table if not exists manufacturers (
     updated_at timestamp not null,
     created_at timestamp not null
 );
+
+--- create 'parts' table
+create table if not exists parts (
+    id int not null primary key generated always as identity,
+    name varchar(255) not null,
+    unit_price decimal(10,2),
+    description varchar(255) null,
+    constraint fk_manufacturer
+        foreign key(id)
+            references manufacturers(id)
+            on delete set null,
+    constraint fk_suppliers
+        foreign key(id)
+            references suppliers(id)
+            on delete set null,
+    updated_at timestamp not null,
+    created_at timestamp not null
+);
+
 
 --- create 'bom' table (for many to many parts and projects resolution)
 create table if not exists bom (
