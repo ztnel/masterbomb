c0ad76ef159a95d9091dcc0728dff00f920473b0
cSDes1gn <christian911@sympatico.ca>
2021-03-31 01:50:16 -0400

    link projects route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import express from 'express';
 import suppliersRouter from './suppliers.routes';
+import projectsRouter from './projects.routes';
 
 const routes = Router();
 
-// connect index router to /suppliers route
+// connect index router to subroutes route
 routes.use('/suppliers', suppliersRouter);
+routes.use('/projects', projectsRouter);
 
 /* GET home page */
 routes.get('/', (_, response:express.Response) => {
