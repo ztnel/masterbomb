093898e27ef30872e9b1e3ab8e47b11bef9abcda
cSDes1gn <christian911@sympatico.ca>
2021-03-31 02:08:08 -0400

    naming fixes

--- src/routes/projects.routes.ts
+++ src/routes/projects.routes.ts
@@ -3,21 +3,16 @@ import { Router } from 'express';
 
 const projectsRouter = Router();
 
-// /** Render suppliers page */
-// projectsRouter.get('/', (_, response:express.Response) => {
-//     response.render('suppliers', {title: 'Suppliers'});
-// });
-
-/** GET /suppliers/all */
+/** GET /projects/all */
 projectsRouter.get('/all', async (request:express.Request, response:express.Response) => {
     try {
         // get database passed by request object
         const db = request.app.get('db');
         // FIX: do not query using * in application runtime, explicitly specify cols to reduce db traffic
-        const suppliers = await db.any(`
+        const projects = await db.any(`
             SELECT * FROM projects`
         );
-        return response.json(suppliers);
+        return response.json(projects);
     } catch (err) {
         console.error(err);
         response.json({error: err.message || err });
