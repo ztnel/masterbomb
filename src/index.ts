18f58c24a0479916fe8c070e621d812f991b9902
cSDes1gn <christian911@sympatico.ca>
2021-04-01 01:39:45 -0400

    small index type optimizations

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import { NextFunction, Response, Request } from 'express';
 import createError from 'http-errors';
 import path from "path";
 import routes from './routes';
 import dotenv from "dotenv";
 import pgPromise from "pg-promise";
 
+// load environment settings
 dotenv.config();
 
 // port is now available to the Node.js runtime
@@ -30,7 +32,7 @@ app.set('view engine', 'ejs');
 app.set('db', db);
 
 // add logger middleware to express app
-app.use((request:express.Request, _, next:express.NextFunction) => {
+app.use((request:Request, _res, next:NextFunction) => {
     console.log(`${request.method} ${request.path}`);
     next();
 });
@@ -46,13 +48,13 @@ app.listen(port, () => {
     console.log(`server started at http://localhost:${ port }`);
 });
 
-app.use((_1, _2, next:express.NextFunction) => {
+app.use((_req, _res, next:NextFunction) => {
     // forward 404 error
     next(createError(404));
 });
 
 // custom error handler
-app.use((err:createError.HttpError, request:express.Request, response:express.Response) => {
+app.use((err:createError.HttpError, request:Request, response:Response) => {
     // set locals, only providing error in development
     response.locals.message = err.message;
     response.locals.error = request.app.get('NODE_ENV') === 'development' ? err : {};
