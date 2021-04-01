a4e7361fbd4900f63467a54a8a50aceabd4ebad7
cSDes1gn <christian911@sympatico.ca>
2021-03-31 22:26:10 -0400

    create parts route

---
+++ src/routes/parts.routes.ts
@@ -0,0 +1,79 @@
+import express from 'express';
+import { Router } from 'express';
+
+const partsRouter = Router();
+
+/** GET /parts/all */
+partsRouter.get('/all', async (request:express.Request, response:express.Response) => {
+    try {
+        // get database passed by request object
+        const db = request.app.get('db');
+        const parts = await db.any(`
+            SELECT name, description, manufacturer_id, supplier_id, unit_price FROM parts`
+        );
+        return response.json(parts);
+    } catch (err) {
+        console.error(err);
+        response.json({error: err.message || err });
+        return false;
+    }
+});
+
+/** GET /parts/get/:id */
+partsRouter.get('/get/:id', async (request:express.Request, response:express.Response) => {
+    try {
+        // get database passed by request object
+        const db = request.app.get('db');
+        const parts = await db.any(`
+            SELECT name, description, manufacturer_id, supplier_id, unit_price FROM parts
+            WHERE id = $[id]`,
+            { id: request.params.id }, (r:any) => r.rowCount
+        );
+        return response.json(parts);
+    } catch (err) {
+        console.error(err);
+        response.json({error: err.message || err });
+        return false;
+    }
+});
+
+/** POST /parts/add */
+partsRouter.post('/add', async (request:express.Request, response:express.Response) => {
+    try {
+        // get database passed by request object
+        const db = request.app.get('db');
+        const id = await db.one(`
+            INSERT INTO parts( name, description, manufacturer_id, supplier_id, unit_price )
+            VALUES( $[name], $[description], $[manufacturer_id], $[supplier_id], $[unit_price] )
+            RETURNING id;`,
+            {...request.body}
+        );
+        return response.status(201).json({ id });
+    } catch (err) {
+        // catch errors and log (returning false)
+        console.error(err);
+        response.json({error: err.message || err });
+        return false;
+    }
+});
+
+/** DELETE /parts/delete */
+partsRouter.delete('/delete/:id', async (request:express.Request, response:express.Response) => {
+    try {
+        // get database passed by request object
+        const db = request.app.get('db');
+        const id = await db.result(`
+            DELETE FROM parts
+            WHERE id = $[id]`,
+            { id: request.params.id }, (r:any) => r.rowCount
+        );
+        return response.json({ id });
+    } catch (err) {
+        // catch errors and log (returning false)
+        console.error(err);
+        response.json({error: err.message || err });
+        return false;
+    }
+});
+
+export default partsRouter;
