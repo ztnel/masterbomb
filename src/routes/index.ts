60e5899b0482eb1f955751ddb36e8b1c05fb1d5a
cSDes1gn <christian911@sympatico.ca>
2021-03-31 22:41:31 -0400

    link /parts to index route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,18 +3,19 @@ import express from 'express';
 import suppliersRouter from './suppliers.routes';
 import manufacturersRouter from './manufacturers.routes';
 import projectsRouter from './projects.routes';
+import partsRouter from './parts.routes';
 
 const routes = Router();
 
 // connect index router to subroutes route
+routes.use('/parts', partsRouter);
 routes.use('/suppliers', suppliersRouter);
 routes.use('/manufacturers', manufacturersRouter);
 routes.use('/projects', projectsRouter);
 
 /* GET home page */
-routes.get('/', (_, response:express.Response) => {
+routes.get('/', (_req, response:express.Response) => {
     response.render('index', { title: 'Masterbomb' });
 });
 
-// export module
 export default routes;
