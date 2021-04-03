0d67b69b989417f10499da4c925149918adf17b9
cSDes1gn <christian911@sympatico.ca>
2021-04-02 23:43:15 -0400

    fix route defs

--- src/api/v1/routes/bom.ts
+++ src/api/v1/routes/bom.ts
@@ -11,19 +11,17 @@ import { BomRequest } from '../interfaces/bom';
 
 const bomRouter = Router();
 
-/** GET /v1/bom/?project_id=X&part_id=Y */
+/** GET /v1/bom/?project_id=<int> */
 bomRouter.get('/', async (request:BomRequest, response:Response):Promise<Response> => {
     // parse query string
-    const part_id = request.query.part_id;
     const project_id = request.query.project_id;
     console.log("Project ID:" + project_id);
-    console.log("Part ID:" + part_id);
     try {
         // get database passed by request object
         const db = request.app.get('db');
-        // FIX: do not query using * in application runtime, explicitly specify cols to reduce db traffic
         const bom = await db.any(`
-            SELECT id, name, description FROM bom`
+            SELECT parts, quantity FROM bom
+            WHERE project_id=$[project_id]`
         );
         response.json(bom);
     } catch (err) {
@@ -38,10 +36,9 @@ bomRouter.post('/', async (request:Request, response:Response):Promise<Response>
     try {
         // get database passed by request object
         const db = request.app.get('db');
-        const id = await db.one(`
-            INSERT INTO bom( name, description )
-            VALUES( $[part_id], $[project_id], $[quantity] )
-            RETURNING id;`,
+        const id = await db.any(`
+            INSERT INTO bom( project_id, part_id, quantity )
+            VALUES( $[part_id], $[project_id], $[quantity] );`,
             {...request.body}
         );
         response.status(201).json({ id });
