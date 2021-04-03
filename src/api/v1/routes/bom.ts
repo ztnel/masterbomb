785bfee0ea7c528b98ad743050865e7816adceb0
cSDes1gn <christian911@sympatico.ca>
2021-04-02 23:53:14 -0400

    fix bom get query

--- src/api/v1/routes/bom.ts
+++ src/api/v1/routes/bom.ts
@@ -14,19 +14,18 @@ const bomRouter = Router();
 /** GET /v1/bom/?project_id=<int> */
 bomRouter.get('/', async (request:BomRequest, response:Response):Promise<Response> => {
     // parse query string
-    const project_id = request.query.project_id;
-    console.log("Project ID:" + project_id);
     try {
         // get database passed by request object
         const db = request.app.get('db');
         const bom = await db.any(`
-            SELECT parts, quantity FROM bom
-            WHERE project_id=$[project_id]`
+            SELECT part_id, quantity FROM bom
+            WHERE project_id=$[project_id]`,
+            {...request.query}
         );
-        response.json(bom);
+        response.status(200).json(bom);
     } catch (err) {
         console.error(err);
-        response.status(500).json({error: err.message || err });
+        response.status(500).json({ error: err.message || err });
     }
     return response;
 });
@@ -38,7 +37,7 @@ bomRouter.post('/', async (request:Request, response:Response):Promise<Response>
         const db = request.app.get('db');
         const id = await db.any(`
             INSERT INTO bom( project_id, part_id, quantity )
-            VALUES( $[part_id], $[project_id], $[quantity] );`,
+            VALUES( $[project_id], $[part_id], $[quantity] );`,
             {...request.body}
         );
         response.status(201).json({ id });
