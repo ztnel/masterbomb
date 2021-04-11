df23365938ce653a4ee1970cfa0c87453a8fa56f
cSDes1gn <christian911@sympatico.ca>
2021-04-11 00:20:11 -0400

    link page routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,4 +8,24 @@ pageRoutes.get('/', (_req, response:express.Response) => {
     response.render('index', { title: 'Masterbomb' });
 });
 
+/* GET parts page */
+pageRoutes.get('/parts', (_req, response:express.Response) => {
+    response.render('parts', { title: 'Parts List' });
+});
+
+/* GET projects page */
+pageRoutes.get('/projects', (_req, response:express.Response) => {
+    response.render('projects', { title: 'Project List' });
+});
+
+/* GET suppliers page */
+pageRoutes.get('/suppliers', (_req, response:express.Response) => {
+    response.render('suppliers', { title: 'Supplier List' });
+});
+
+/* GET manufacturers page */
+pageRoutes.get('/manufacturers', (_req, response:express.Response) => {
+    response.render('manufacturers', { title: 'Manufacturers List' });
+});
+
 export default pageRoutes;
