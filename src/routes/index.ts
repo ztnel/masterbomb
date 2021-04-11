384c400747f0516eab89c2da4be6b54a9045b0f3
cSDes1gn <christian911@sympatico.ca>
2021-04-11 00:36:43 -0400

    update page routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,27 +5,27 @@ const pageRoutes = Router();
 
 /* GET home page */
 pageRoutes.get('/', (_req, response:express.Response) => {
-    response.render('index', { title: 'Masterbomb' });
+    response.render('pages/index', { title: 'Masterbomb' });
 });
 
 /* GET parts page */
 pageRoutes.get('/parts', (_req, response:express.Response) => {
-    response.render('parts', { title: 'Parts List' });
+    response.render('pages/parts', { title: 'Parts List' });
 });
 
 /* GET projects page */
 pageRoutes.get('/projects', (_req, response:express.Response) => {
-    response.render('projects', { title: 'Project List' });
+    response.render('pages/projects', { title: 'Project List' });
 });
 
 /* GET suppliers page */
 pageRoutes.get('/suppliers', (_req, response:express.Response) => {
-    response.render('suppliers', { title: 'Supplier List' });
+    response.render('pages/suppliers', { title: 'Supplier List' });
 });
 
 /* GET manufacturers page */
 pageRoutes.get('/manufacturers', (_req, response:express.Response) => {
-    response.render('manufacturers', { title: 'Manufacturers List' });
+    response.render('pages/manufacturers', { title: 'Manufacturers List' });
 });
 
 export default pageRoutes;
