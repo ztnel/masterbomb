3fddaa9ea97000cd9a1a2cf86c2632157ca2dc23
cSDes1gn <christian911@sympatico.ca>
2021-04-11 21:08:42 -0400

    axios and ejs render

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import express from 'express';
+import axios from 'axios';
+import { ISuppliers } from 'api/v1/interfaces/suppliers';
 
 const pageRoutes = Router();
 
@@ -26,10 +28,16 @@ pageRoutes.get('/projects', (_req, response:express.Response) => {
 /* GET suppliers page */
 pageRoutes.get('/suppliers', (_req, response:express.Response) => {
     // axios request for all suppliers
+    axios.get<ISuppliers[]>("http://localhost:3000/v1/suppliers", {
+        headers: {"Content-Type": "application/json"}
+    }).then(axios_response => {
+        console.log("axios response: ", axios_response);
+        // pass ejs the supplier list
+        response.render('pages/suppliers',
+            { title: 'Suppliers', suppliers: axios_response.data }
+        );
+    });
 
-    // pass ejs the supplier list
-    response.render('pages/suppliers',
-        { title: 'Suppliers', suppliers: supplier_list });
 });
 
 /* GET manufacturers page */
