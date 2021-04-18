dec415681447ec2cfeb787b160fb1d568ea00f4a
cSDes1gn <christian911@sympatico.ca>
2021-04-18 12:29:24 -0400

    added deletion functionality

--- src/public/js/suppliers.js
+++ src/public/js/suppliers.js
@@ -2,6 +2,8 @@
 const suppliers_endpoint = '/v1/suppliers/';
 var suppliers = [];
 var $table = $('#suppliersTable');
+var $remove = $('#deleteSupplier');
+var selections = [];
 
 // DOM Ready
 $(() => {
@@ -10,13 +12,27 @@ $(() => {
     // Add supplier modal click
     $('#addSupplier').on('click', add);
     $('#postSupplier').on('click', post_supplier);
-    $('#deleteSupplier').on('click', delete_supplier);
+    $remove.on('click', delete_supplier);
+    $remove.prop('disabled', true);
+    $table.on('check.bs.table uncheck.bs.table ' + 'check-all.bs.table uncheck-all.bs.table',function () {
+        $remove.prop('disabled', !$table.bootstrapTable('getSelections').length);
+        // save your data, here just save the current page
+        selections = get_id_selection();
+        console.log(selections);
+    })
 });
+
+// get the ids of all selected elements
+function get_id_selection () {
+    return $.map($table.bootstrapTable('getSelections'), function (row) {
+        return row.id;
+    })
+}
 
 // populate table with current state
 function getState() {
+    $table.bootstrapTable("destroy");
     console.log("Fetching supplier table state");
-    var table_content = '';
     // ajax call to suppliers api
     console.log(`API GET ${suppliers_endpoint}`);
     $.get({
@@ -26,7 +42,7 @@ function getState() {
         suppliers = data.reverse();
         console.log("Response: ", suppliers);
         // inject html for table
-        $table.bootstrapTable({data: suppliers})
+        $table.bootstrapTable({data: suppliers});
     }).catch(() => {
         console.error("API request failed");
         alert("API Request Failed");
@@ -81,22 +97,25 @@ function post_supplier(event) {
 // post new supplier
 function delete_supplier(event) {
     event.preventDefault();
+    var ids = get_id_selection();
     // start delete request
-    const newSupplier = {
-        'name': $('#supplierForm #supplierName').val(),
-        'website': $('#supplierForm #supplierWebsite').val()
-    };
-    let endpoint = `${suppliers_endpoint}/${id}`;
-    console.log(`API DELETE ${endpoint}`);
-    $.delete({
-        data: newSupplier,
-        url: suppliers_endpoint,
-        dataType:'JSON'
-    }).then(() => {
-       // rerequest list
-       getState();
-    }).catch(() => {
-        console.error("API request failed");
-        alert("API Request Failed");
-    })
+    ids.forEach(id => {
+        let endpoint = `${suppliers_endpoint}${id}`;
+        console.log(`API DELETE ${endpoint}`);
+        $.ajax({
+            url: endpoint,
+            type: 'DELETE',
+            dataType: 'json',
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(response) {
+                console.log(response);
+                console.error("API request failed");
+                alert("API Request Failed");
+            }
+        });
+    });
+    getState();
+    $remove.prop('disabled', true);
 }
