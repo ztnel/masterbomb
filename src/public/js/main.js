d415d800f5c23afa628b14c6c375afd4b5a16d5f
cSDes1gn <christian911@sympatico.ca>
2021-04-17 23:51:42 -0400

    update ejs templates

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,14 +1,17 @@
+// DOM Ready
 $(() => {
+    console.log("Hello");
+    // $('#supplierModal').modal(options)
     // Add supplier modal click
     $('#submitSupplier').on('click', add);
 });
 
 function add(event) {
     console.log("test");
-    // event.preventDefault();
-    // // form validation
-    // var error_flag = false;
-    // $('#addSupplier input')
-    // var table_content = '';
-    // $getJSON()
+    event.preventDefault();
+    // form validation
+    var error_flag = false;
+    $('#addSupplier input')
+    var table_content = '';
+    $getJSON()
 }
