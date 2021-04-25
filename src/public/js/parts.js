0ef683f0a140a213e916be39079d9b85d83c4190
cSDes1gn <christian911@sympatico.ca>
2021-04-25 16:21:13 -0400

    searchable selectors fix state managment

--- src/public/js/parts.js
+++ src/public/js/parts.js
@@ -1,5 +1,7 @@
 // global parts state
 const parts_endpoint = '/v1/parts/';
+const suppliers_endpoint = '/v1/suppliers/'
+const manufacturers_endpoint = '/v1/manufacturers/'
 var parts = [];
 var suppliers = [];
 var manufacturers = [];
@@ -9,6 +11,8 @@ var $add = $('#addPart');
 var $edit = $('#editPart');
 var $post = $('#post');
 var $put = $('#put');
+var $supplier_selector = $('#supplier-picker');
+var $manufacturer_selector = $('#manufacturer-picker');
 var selections = [];
 
 // DOM Ready
@@ -20,6 +24,8 @@ $(() => {
     $edit.on('click', edit); 
     $put.on('click', put_part);
     $remove.on('click', delete_part);
+    $supplier_selector.selectpicker();
+    $manufacturer_selector.selectpicker();
     // register table events
     $table.on('check.bs.table uncheck.bs.table ' + 'check-all.bs.table uncheck-all.bs.table',function () {
         $remove.prop('disabled', !$table.bootstrapTable('getSelections').length);
@@ -35,17 +41,6 @@ $(() => {
     get_state();
 });
 
-// get the ids of all selected elements
-function get_id_selection () {
-    return $.map($table.bootstrapTable('getSelections'), function (row) {
-        return row.id;
-    })
-}
-
-function loading_template() {
-    return '<div class="spinner-border text-light" role="status"><span class="sr-only">Loading...</span></div>'
-}
-
 // populate table with current state
 function get_state() {
     $table.bootstrapTable('showLoading');
@@ -58,23 +53,17 @@ function get_state() {
         // update global state
         parts = data.reverse();
         console.log("Response: ", parts);
-        let suppliers_set = new Set(), manufacturers_set = new Set();
-        parts.forEach(part => {
-            !part.supplier_id ? console.warn(`Supplier ID for ${part} is null, skipping.`): suppliers_set.add(part.supplier_id);
-            !part.manufacturer_id ? console.warn(`Manufacturer ID for ${part} is null, skipping.`): manufacturers_set.add(part.manufacturer_id);
-        });
         // update global state with supplier and manufacturers
-        update_fks(suppliers_set, manufacturers_set).then( () =>{
+        get_fks().then( () =>{
             // replace ids in parts table with name identifiers
-            console.log(suppliers);
-            console.log(manufacturers);
             parts.forEach(part => {
-                part.manufacturer = !is_empty(manufacturers) ? manufacturers.filter((mf) => {return (mf.id === part.manufacturer_id)})[0].name : undefined
-                part.supplier = !is_empty(suppliers) ? suppliers.filter((s) => {return (s.id === part.supplier_id)})[0].name : undefined
+                part.manufacturer = find_match(manufacturers, part.manufacturer_id);
+                part.supplier = find_match(suppliers, part.supplier_id);
             });
             // temp timeout for load wheel debug
             setTimeout(() => {$table.bootstrapTable('load', parts)}, 1000);
             console.log("Compiled Response: ", parts);
+            populate_selectors();
         });
     }).catch(() => {
         console.error("API request failed");
@@ -85,44 +74,50 @@ function get_state() {
     $edit.prop('disabled', true);
 }
 
-function is_empty(arr) {
-    return arr === undefined || arr.length == 0;
+function find_match(arr, id) {
+    if (arr === undefined || arr.length == 0 || id === null) {
+        return undefined;
+    }
+    return arr.filter((el) => {return (el.id === id)})[0].name
 }
 
-async function update_fks(suppliers_set, manufacturers_set) {
-    let suppliers_endpoint = '/v1/suppliers/'
-    let manufacturers_endpoint = '/v1/manufacturers/'
+// get the ids of all selected elements
+function get_id_selection () {
+    return $.map($table.bootstrapTable('getSelections'), function (row) {
+        return row.id;
+    })
+}
+
+function loading_template() {
+    return '<div class="spinner-border text-light" role="status"><span class="sr-only">Loading...</span></div>'
+}
+
+async function get_fks() {
     // compile ajax queries and end with promise.all
     let promises = []
-    // fetch supplier names with id
-    suppliers_set.forEach(id => {
-        console.log(`API GET ${suppliers_endpoint}${id}`);
-        promises.push(
-            $.get({
-                url: `${suppliers_endpoint}${id}`
-            }).then((data) => {
-                suppliers.push(data);
-                console.log("Response: ", suppliers);
-            }).catch(() => {
-                console.error("API request failed");
-            })
-        );
-    })
-    // fetch manufacturer names with id
-    manufacturers_set.forEach(id => {
-        console.log(`API GET ${manufacturers_endpoint}${id}`);
-        promises.push(
-            $.get({
-                url: `${manufacturers_endpoint}${id}`
-            }).then((data) => {
-                manufacturers.push(data);
-                console.log("Response: ", manufacturers);
-            }).catch(() => {
-                console.error("API request failed");
-            })
-        );
-    })
-    return Promise.all(promises).then( () => {});
+    console.log(`API GET ${suppliers_endpoint}`);
+    promises.push(
+        $.get({
+            url: `${suppliers_endpoint}`
+        }).then((data) => {
+            suppliers = data;
+            console.log("Response: ", suppliers);
+        }).catch(() => {
+            console.error("API request failed");
+        })
+    );
+    console.log(`API GET ${manufacturers_endpoint}`);
+    promises.push(
+        $.get({
+            url: `${manufacturers_endpoint}`
+        }).then((data) => {
+            manufacturers = data;
+            console.log("Response: ", manufacturers);
+        }).catch(() => {
+            console.error("API request failed");
+        })
+    );
+    return Promise.all(promises);
 }
 
 // spawn part form modal
@@ -148,8 +143,12 @@ function edit(event) {
             $('#partName').val(part.name);
             $('#partDesc').val(part.description);
             $('#partUnitPrice').val(part.unit_price);
+            $manufacturer_selector.val(part.manufacturer_id);
+            $supplier_selector.val(part.supplier_id);
         }
     });
+    $manufacturer_selector.selectpicker('refresh');
+    $supplier_selector.selectpicker('refresh');
     $post.prop("hidden", true);
     $put.prop("hidden", false);
     // set title
@@ -157,25 +156,49 @@ function edit(event) {
     $('#formModal').modal();
 }
 
+function populate_selectors() {
+    // clear selectors
+    $supplier_selector.empty('#supplier-picker');
+    $manufacturer_selector.empty('#manufacturer-picker');
+    // populate select pickers
+    for (let idx in suppliers) {
+       $supplier_selector.append($('<option />').val(suppliers[idx].id).text(suppliers[idx].name));
+    }
+    for (let idx in manufacturers) {
+       $manufacturer_selector.append($('<option />').val(manufacturers[idx].id).text(manufacturers[idx].name));
+    }
+    $supplier_selector.selectpicker(); 
+    $manufacturer_selector.selectpicker(); 
+    $supplier_selector.selectpicker('refresh');
+    $manufacturer_selector.selectpicker('refresh');
+}
+
 // post new part
 function post_part(event) {
     console.log("Validating part form");
     event.preventDefault();
     // basic form validation
-    var error_flag = false;
-    $('#partForm input').each(function (index, val) {
-        if ($(this).val() === '') {
-            error_flag = true;
-        }
-    });
-    if (error_flag == true) {
-        console.error("Validation failed");
-        alert("Enter all required fields");
-        return false;
-    }
+    // var error_flag = false;
+    // $('#partForm input').each(function (index, val) {
+    //     if ($(this).val() === '') {
+    //         error_flag = true;
+    //     }
+    // });
+    // if (!$('#supplier-picker').val()) {
+    //     error_flag = true;
+    // }
+    // if (error_flag == true) {
+    //     console.error("Validation failed");
+    //     alert("Enter all required fields");
+    //     return false;
+    // }
     // start post request
     const part_payload = {
-        'name': $('#partForm #partName').val()
+        'name': $('#partForm #partName').val(),
+        'description': $('#partForm #partDesc').val(),
+        'manufacturer_id': $('#partForm #manufacturer-picker').val(),
+        'supplier_id': $('#partForm #supplier-picker').val(),
+        'unit_price': $('#partForm #partUnitPrice').val()
     };
     console.log(`API POST ${parts_endpoint} with: `, {...part_payload});
     $.post({
@@ -185,6 +208,9 @@ function post_part(event) {
     }).then(() => {
        // clear fields
        $('#partForm input').val('');
+       $('#partForm textarea').val('');
+       $manufacturer_selector.selectpicker();
+       $supplier_selector.selectpicker();
        // hide modal
        $('#formModal').modal('toggle');
        // rerequest get requests
@@ -200,21 +226,25 @@ function put_part(event) {
     console.log("Validating part form");
     event.preventDefault();
     // basic form validation
-    var error_flag = false;
-    $('#partForm input').each(function (index, val) {
-        if ($(this).val() === '') {
-            error_flag = true;
-        }
-    });
-    if (error_flag == true) {
-        console.error("Validation failed");
-        alert("Enter all required fields");
-        return false;
-    }
+    // var error_flag = false;
+    // $('#partForm input').each(function (index, val) {
+    //     if ($(this).val() === '') {
+    //         error_flag = true;
+    //     }
+    // });
+    // if (error_flag == true) {
+    //     console.error("Validation failed");
+    //     alert("Enter all required fields");
+    //     return false;
+    // }
     // here only a single id field can be selected so this getter is safe
     const part_payload = {
         'id': get_id_selection()[0],
         'name': $('#partForm #partName').val(),
+        'description': $('#partForm #partDesc').val(),
+        'manufacturer_id': $('#partForm #manufacturer-picker').val(),
+        'supplier_id': $('#partForm #supplier-picker').val(),
+        'unit_price': $('#partForm #partUnitPrice').val()
     };
     console.log(`API POST ${parts_endpoint} with: `, {...part_payload});
     $.ajax({
